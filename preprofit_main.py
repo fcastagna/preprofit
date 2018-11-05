8fa0bc0a7b7a73e360e9043e474063575f3c8991
fcastagna <44572424+fcastagna@users.noreply.github.com>
2018-11-05 15:28:21 +0100

    Update preprofit_main.py

--- preprofit_main.py
+++ preprofit_main.py
@@ -110,9 +110,8 @@ filtering = f(karr)
 
 # Flux density data
 data = np.loadtxt(flux_filename, skiprows = 1, unpack = True)
-r_sec = np.linspace(0.5, max(data[0]), max(data[0])) * 0.0017425 * 3600
-# 0.0017425 * 3600 is the step in r [arcsec]
-y_data = data[1] / data[2] / compt_param_mJy # Jy / beam to Compton parameter
+r_sec = data[0]
+y_data = data[1] / data[2] / compt_param_mJy # Jy/beam to Compton parameter
 err = data[3] # TBI statistical error
 flux_data = [r_sec, y_data, err]
 
