fe71bd4aeb39865462556b284e713c54e0c38a81
fcastagna <44572424+fcastagna@users.noreply.github.com>
2019-03-22 14:45:34 +0100

    Update preprofit_main.py

--- preprofit_main.py
+++ preprofit_main.py
@@ -37,20 +37,20 @@ nwalkers = 100
 nthreads = 8
 nburn = 1000
 nsteps = 1000
-np.random.seed(0)
+np.random.seed(0) # optionally, we set a random seed
 ci = 95 # confidence interval level
 
 
 ### Local variables
 
 redshift = 0.888
-compt_param_mJy = -10.9 * 10**3 # Compton parameter to Jy/beam
+compt_param_mJy = -10.9 * 10**3 # Compton parameter to Jy/beam conversion factor
 
 # File names
 files_dir = 'data'
+flux_filename = '%s/press_data_Adam.dat' % files_dir
 beam_filename = '%s/Beam150GHz.fits' % files_dir
 tf_filename = '%s/TransferFunction150GHz_CLJ1227.fits' % files_dir
-flux_filename = '%s/press_data_Adam.dat' % files_dir
 
 # Cosmological parameters
 cosmology = mb.Cosmology(redshift)
@@ -69,34 +69,34 @@ for i in name_pars:
         pars[i].frozen = True
 
 # Flux density data
-flux_data = np.loadtxt(flux_filename, skiprows = 1, unpack = True) # radius (arcsec), flux density, TBI statistical error
+flux_data = np.loadtxt(flux_filename, skiprows = 1, unpack = True) # radius (arcsec), flux density, statistical error
 
 # Radius definition
-mymaxr = 60 * (np.ceil(flux_data[0][-1] / 60)) # max radius needed
-# here we set it to at least 1 arcmin more than the highest x-value in the data
+mymaxr = 60 * (np.ceil(flux_data[0][-1] / 60)) # max radius needed (arcsec)
+# here we set it to the integer unit of arcmin that follows the highest x-value in the data
 radius = np.arange(0, mymaxr + mystep, mystep) # array of radii in arcsec
 rad_kpc = radius * kpc_per_arcsec # radius in kpc
 radius = np.append(-radius[:0:-1], radius) # from positive to entire axis
 sep = radius.size // 2 # index of radius 0
 
-# Y 2D matrix
+# Basic matrix to represent the 2D image of the integrated Compton parameter
 y_mat = centdistmat(mystep, max_dist = mymaxr)
 
-# PSF read, regularize and image creation
+# PSF computation and creation of the 2D image
 beam, fwhm_beam = mybeam(beam_filename, radius, regularize = True)
 beam_mat = centdistmat(mystep, max_dist = 3 * fwhm_beam) 
 # here we use 3 times the FWHM of the PSF as the maximum distance for half-side of the image
 beam_2d = ima_interpolate(beam_mat, radius, beam)
 
 # Transfer function
-tf_mat_len = y_mat.shape[0] // 2 # one side length of the tf image
+tf_mat_side = y_mat.shape[0] # one side length of the tf image
 reso = fwhm_beam # resolution of the map in arcsec (= FWHM of the PSF)
 kmax = 1 / reso
-karr = dist(tf_mat_len)
+karr = dist(tf_mat_side)
 karr = karr / np.max(karr) * kmax
-tf_data = fits.open(tf_filename)[1].data[0]
+tf_data = fits.open(tf_filename)[1].data[0] # transfer function data
 wn_as = tf_data[0] # wave number in arcsec^(-1)
-tf = tf_data[1]
+tf = tf_data[1] # transmission
 f = interp1d(wn_as, tf, fill_value = 'extrapolate') # tf interpolation
 filtering = f(karr)
 
@@ -110,7 +110,7 @@ starting_var = np.array(np.repeat(.1, ndim))
 starting_guesses = np.random.random((nwalkers, ndim)) * starting_var + starting_guess
 sampler = emcee.EnsembleSampler(nwalkers, ndim, log_lik, args = [
     press, pars, fit_pars, mystep, kpc_per_arcsec, phys_const, radius, y_mat, 
-    beam_2d, filtering, tf_len, sep, flux_data, convert], threads = nthreads)
+    beam_2d, filtering, sep, flux_data, convert], threads = nthreads)
 print('Starting burn-in')
 for i, result in enumerate(sampler.sample(starting_guesses, iterations = nburn,
                                           storechain = False)):
@@ -150,7 +150,7 @@ triangle(mysamples, fit_pars, plotdir)
 
 # Random samples of at most 1000 profiles
 out_prof = np.array([fit(mysamples[j], press, pars, fit_pars, mystep, kpc_per_arcsec, phys_const, radius, y_mat, beam_2d, 
-                         filtering, tf_len, sep, flux_data, convert) for j in 
+                         filtering, sep, flux_data, convert) for j in 
                      np.random.choice(mysamples.shape[0], size = min(1000, mysamples.shape[0]), replace = False)])
 quant = np.percentile(out_prof, [50, 50 - ci / 2, 50 + ci / 2], axis = 0)
 
