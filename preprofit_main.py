8d9e6f704a146ee2ec4020b47141d3d13de558eb
fcastagna <44572424+fcastagna@users.noreply.github.com>
2019-05-15 14:51:40 +0200

    Update preprofit_main.py

--- preprofit_main.py
+++ preprofit_main.py
@@ -80,8 +80,8 @@ radius = np.append(-radius[:0:-1], radius) # from positive to entire axis
 sep = radius.size//2 # index of radius 0
 r_pp = np.arange(mystep*kpc_as, 5*1000+mystep*kpc_as, mystep*kpc_as) # radius in kpc used to compute the pressure profile
 
-# Matrix of distances centered on 0 with step=mystep
-d_mat = centdistmat(radius)
+# Matrix of distances in kpc centered on 0 with step=mystep
+d_mat = centdistmat(radius*kpc_as)
 
 # PSF computation and creation of the 2D image
 beam_2d, fwhm_beam = mybeam(radius, filename=beam_filename, regularize=True)
