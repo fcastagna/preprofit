962c7a3946b5eaebea4ac80fb8bf86ddeaba68ca
fcastagna <44572424+fcastagna@users.noreply.github.com>
2019-08-05 18:04:33 +0200

    Update preprofit_funcs.py

--- preprofit_funcs.py
+++ preprofit_funcs.py
@@ -328,12 +328,13 @@ def triangle(mysamples, param_names, plotdir='./'):
     pdf.savefig()
     pdf.close()
     
-def plot_best(theta, fit_pars, mp_med, mp_lb, mp_ub, radius, sep, flux_data, ci, plotdir='./'):
+def plot_best(theta, fit_pars, mp_med, mp_lb, mp_ub, radius, sep, flux_data, ci=95, plotdir='./'):
     '''
     Plot of the Compton parameter profile compared to the flux density data
     -----------------------------------------------------------------------
     mp_med = Compton profile for the median parameter values
     mp_lb, mp_ub = CI of the Compton profile
+    ci = confidence interval
     plotdir = directory where to place the plot
     '''
     r_sec, y_data, err = flux_data
@@ -342,11 +343,10 @@ def plot_best(theta, fit_pars, mp_med, mp_lb, mp_ub, radius, sep, flux_data, ci,
     plt.plot(radius[sep:sep+mp_med.size], mp_med)
     plt.fill_between(radius[sep:sep+mp_med.size], mp_lb, mp_ub, color='powderblue', label='_nolegend_')
     plt.errorbar(r_sec, y_data, yerr=err, fmt='.', color='r')
-    plt.legend(('Filtered profile with %s%% CI' %ci, 'Flux density'), loc='lower right')
+    plt.legend(('Model (%i%% CI)' %ci, 'Observed flux'), loc='lower right')
     plt.axhline(y=0, color='black', linestyle=':')
     plt.xlabel('Radius (arcsec)')
     plt.ylabel('Flux (mJy/beam)')
     plt.xlim(0, np.ceil(r_sec[-1]/60)*60*7/6)
-    plt.title('Flux density profile: best-fit with %s%% CI' %ci)
     pdf.savefig()
     pdf.close()
