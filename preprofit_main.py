a306e8054be867dd4649db11429dc86a520de524
Fabio Castagna <fabio.castagna@inaf.it>
2020-06-05 18:29:09 +0200

    Update preprofit_main.py

--- preprofit_main.py
+++ preprofit_main.py
@@ -1,11 +1,10 @@
-from preprofit_funcs import Pressure, mybeam, centdistmat, read_tf, filt_image, log_lik, mcmc_run, traceplot, triangle, plot_best
+from preprofit_funcs import Pressure, read_xy_err, mybeam, centdistmat, read_tf, filt_image, log_lik, mcmc_run
+from preprofit_plots import traceplot, triangle, plot_best
 import numpy as np
 import mbproj2 as mb
 from scipy.interpolate import interp1d
 import emcee
 import six.moves.cPickle as pickle
-
-plotdir = './' # plot directory
 
 
 ### Global-global variables
@@ -24,8 +23,35 @@ press = Pressure()
 pars = press.defPars()
 name_pars = list(pars.keys())
 
+# name for outputs
+name = 'preprofit'
+plotdir = './' # directory for the plots
+savedir = './' # directory for saved files
+
+# Uncertainty level
+ci = 95
+
+# MCMC parameters
+nburn = 2000 # number of burn-in iterations
+nlength = 5000 # number of chain iterations (after burn-in)
+nwalkers = 30 # number of random walkers
+nthreads = 8 # number of processes/threads
+nthin = 5 # thinning
+seed = None # random seed
+
+
+### Local variables
+
+# Cluster cosmology
+redshift = 0.888
+cosmology = mb.Cosmology(redshift)
+cosmology.H0 = 67.32 # Hubble's constant (km/s/Mpc)
+cosmology.WM = 0.3158 # matter density
+cosmology.WV = 0.6842 # vacuum density
+kpc_as = cosmology.kpc_per_arcsec # number of kpc per arcsec
+
 # Parameters that we want to fit (among P_0, r_p, a, b, c)
-fit_pars = ['P_0', 'r_p']
+fit_pars = ['P_0', 'r_p', 'a', 'b']
 # To see the default parameter space extent, use: print(pars)
 # For each parameter, use the following to change the bounds of the prior distribution:
 #pars['P_0'].minval = 0.1
@@ -34,19 +60,6 @@ fit_pars = ['P_0', 'r_p']
 # Sampling step
 mystep = 2. # constant step in arcsec (values higher than 1/3 * FWHM of the PSF are not recommended)
 
-# MCMC parameters
-ndim = len(fit_pars)
-nwalkers = 14
-nthreads = 8
-nburn = 3000
-nsteps = 2000
-np.random.seed(0) # optionally, we set a random seed
-ci = 95. # confidence interval level
-
-
-### Local variables
-
-redshift = 0.888
 R_b = 5000 # Radial cluster extent (kpc), serves as upper bound for Compton y parameter integration
 t_const = 12 # constant value of temperature of the cluster (keV), serves for Compton y to mJy/beam conversion
 
@@ -55,14 +68,18 @@ files_dir = './data' # directory
 beam_filename = '%s/Beam150GHz.fits' %files_dir
 tf_filename = '%s/TransferFunction150GHz_CLJ1227.fits' %files_dir
 flux_filename = '%s/press_clj1226_flagsource.dat' %files_dir
-compt_convert_name = '%s/Compton_to_Jy_per_beam.dat' %files_dir # conversion Compton -> Jy/beam
+convert_filename = '%s/Compton_to_Jy_per_beam.dat' %files_dir # conversion Compton -> Jy/beam
 
-# Cosmological parameters
-cosmology = mb.Cosmology(redshift)
-cosmology.H0 = 67.11 # Hubble's constant (km/s/Mpc)
-cosmology.WM = 0.3175 # matter density
-cosmology.WV = 0.6825 # vacuum density
-kpc_as = cosmology.kpc_per_arcsec # number of kpc per arcsec
+# Beam and transfer function. From raw data or Gaussian approximation?
+beam_approx = False
+tf_approx = False
+fwhm_beam = None # fwhm of the normal distribution for the beam approximation
+loc, scale, c = None, None, None # location, scale and normalization parameters of the normal cdf for the tf approximation
+
+# Integrated Compton parameter option
+calc_integ = False # apply or do not apply?
+integ_mu = .94/1e3 # from Planck 
+integ_sig = .36/1e3 # from Planck
 
 # -------------------------------------------------------------------------------------------------------------------------------
 # Code
@@ -74,31 +91,30 @@ for i in name_pars:
         pars[i].frozen = True
 
 # Flux density data
-flux_data = np.loadtxt(flux_filename, skiprows=1, unpack=True) # radius (arcsec), flux density, statistical error
+flux_data = read_xy_err(flux_filename, ncol=3) # radius (arcsec), flux density, statistical error
 maxr_data = flux_data[0][-1] # highest radius in the data
 
 # PSF computation and creation of the 2D image
-beam_2d, fwhm_beam = mybeam(mystep, maxr_data, filename=beam_filename, normalize=True)
+beam_2d, fwhm = mybeam(mystep, maxr_data, approx=beam_approx, filename=beam_filename, normalize=True, fwhm_beam=fwhm_beam)
 
 # Radius definition
-mymaxr = (maxr_data+3*fwhm_beam)//mystep*mystep # max radius needed (arcsec)
-radius = np.arange(0, mymaxr+mystep, mystep) # array of radii in arcsec
+mymaxr = (maxr_data+3*fwhm)//mystep*mystep # max radius needed (arcsec)
+radius = np.arange(0., mymaxr+mystep, mystep) # array of radii in arcsec
 radius = np.append(-radius[:0:-1], radius) # from positive to entire axis
 sep = radius.size//2 # index of radius 0
 r_pp = np.arange(mystep*kpc_as, R_b+mystep*kpc_as, mystep*kpc_as) # radius in kpc used to compute the pressure profile
-ub = min(sep, r_pp.size) # ub=sep unless r500 is too low and then r_pp.size < sep
 
 # Matrix of distances in kpc centered on 0 with step=mystep
 d_mat = centdistmat(radius*kpc_as)
 
 # Transfer function
-wn_as, tf = read_tf(tf_filename) # wave number in arcsec^(-1), transmission
-filtering = filt_image(wn_as, tf, d_mat.shape[0], mystep)
+wn_as, tf = read_tf(tf_filename, approx=tf_approx, loc=loc, scale=scale, c=c) # wave number in arcsec^(-1), transmission
+filtering = filt_image(wn_as, tf, d_mat.shape[0], mystep) # transfer function matrix
 
 # Compton parameter to mJy/beam conversion
-t_keV, compt_Jy_beam = np.loadtxt(compt_convert_name, skiprows=1, unpack=True)
-conv_mJy_beam = interp1d(t_keV, compt_Jy_beam*1e3, 'linear', fill_value='extrapolate')
-compt_mJy_beam = conv_mJy_beam(t_const) # we assume a constant value of temperature
+t_keV, compt_Jy_beam = np.loadtxt(convert_filename, skiprows=1, unpack=True)
+convert = interp1d(t_keV, compt_Jy_beam*1e3, 'linear', fill_value='extrapolate')
+compt_mJy_beam = convert(t_const) # we assume a constant value of temperature
 
 # Bayesian fit
 starting_guess = [pars[i].val for i in fit_pars]
