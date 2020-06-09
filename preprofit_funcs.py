106b27a9583c3c13d217172e2da1b3da8ecfca12
Fabio Castagna <fabio.castagna@inaf.it>
2020-06-09 12:19:57 +0200

    Update preprofit_funcs.py

--- preprofit_funcs.py
+++ preprofit_funcs.py
@@ -9,10 +9,7 @@ from scipy.signal import fftconvolve
 from scipy.fftpack import fft2, ifft2
 from scipy.optimize import minimize
 import time
-
-font = {'size': 8}
-plt.rc('font', **font)
-plt.style.use('classic')
+import h5py
 
 class Param:
     '''
@@ -43,7 +40,8 @@ class Pressure:
     -----------------------------------------    
     '''
     def __init__(self):
-        pass
+        self.pars = self.defPars()
+#        pass
         
     def defPars(self):
         '''
@@ -54,40 +52,50 @@ class Pressure:
         b = logarithmic slope at r/r_p >> 1
         c = logarithmic slope at r/r_p << 1
         r_p = characteristic radius
-        '''        
+        '''
         pars = {
-            'P_0': Param(0.4, minval=0., maxval=20.),
-            'a': Param(1.33, minval=0.1, maxval=10.),
-            'b': Param(4.13, minval=0.1, maxval=15.),
-            'c': Param(0.014, minval=0., maxval=3.),
+            'P_0': Param(0.4, minval=0., maxval=2.),
+            'a': Param(1.33, minval=0., maxval=10.),
+            'b': Param(4.13, minval=1., maxval=10.),
+            'c': Param(0.014, minval=0., maxval=2.),
             'r_p': Param(300., minval=100., maxval=1000.)
             }
         return pars
 
-    def update_vals(self, pars, fit_pars, pars_val):
+    def update_vals(self, fit_pars, pars_val):
         '''
         Update the parameter values
         ---------------------------
-        pars = set of pressure parameters
         fit_pars = name of the parameters to update
         pars_val = new parameter values
         '''
         for name, i in zip(fit_pars, range(len(fit_pars))):
-            pars[name].val = pars_val[i] 
+            self.pars[name].val = pars_val[i] 
 
-    def press_fun(self, pars, r_kpc):
+    def press_fun(self, r_kpc):
         '''
         Compute the gNFW pressure profile
         ---------------------------------
-        pars = set of pressure parameters
         r_kpc = radius (kpc)
         '''
-        P_0 = pars['P_0'].val
-        a = pars['a'].val
-        b = pars['b'].val
-        c = pars['c'].val
-        r_p = pars['r_p'].val
+        P_0 = self.pars['P_0'].val
+        a = self.pars['a'].val
+        b = self.pars['b'].val
+        c = self.pars['c'].val
+        r_p = self.pars['r_p'].val
         return P_0/((r_kpc/r_p)**c*(1+(r_kpc/r_p)**a)**((b-c)/a)) 
+
+def centdistmat(r, offset=0.):
+    '''
+    Create a symmetric matrix of distances from the radius vector
+    -------------------------------------------------------------
+    r = vector of negative and positive distances with a given step (center value has to be 0)
+    offset = value to be added to every distance in the matrix (default is 0)
+    ---------------------------------------------
+    RETURN: the matrix of distances centered on 0
+    '''
+    x, y = np.meshgrid(r, r)
+    return np.sqrt(x**2+y**2)+offset
 
 def read_xy_err(filename, ncol):
     '''
@@ -102,18 +110,18 @@ def read_xy_err(filename, ncol):
     else:
         raise RuntimeError('Unrecognised file extension (not in fits, dat, txt)')
     return data[:ncol]
-
+    
 def read_beam(filename):
     '''
     Read the beam data from the specified file up to the first negative or nan value
     --------------------------------------------------------------------------------
     '''
     radius, beam_prof = read_xy_err(filename, ncol=2)
-    if np.isnan(beam_prof).sum() > 0.:
+    if np.isnan(beam_prof).sum() > 0:
         first_nan = np.where(np.isnan(beam_prof))[0][0]
         radius = radius[:first_nan]
         beam_prof = beam_prof[:first_nan]
-    if beam_prof.min() < 0.:
+    if beam_prof.min() < 0:
         first_neg = np.where(beam_prof < 0)[0][0]
         radius = radius[:first_neg]
         beam_prof = beam_prof[:first_neg]
@@ -125,20 +133,20 @@ def mybeam(step, maxr_data, approx=False, filename=None, normalize=True, fwhm_be
     --------------------------------------------------------------------------------------------------------
     step = binning step
     maxr_data = highest radius in the data
-    approx = whether to approximate or not the beam to the normal distribution (boolean, default is False)
+    approx = whether to approximate or not the beam to the normal distribution (True/False)
     filename = name of the file including the beam data
-    normalize = whether to normalize or not the output 2D image (boolean, default is True)
+    normalize = whether to normalize or not the output 2D image (True/False)
     fwhm_beam = Full Width at Half Maximum
     -------------------------------------------------------------------
     RETURN: the 2D image of the beam and his Full Width at Half Maximum
     '''
     if not approx:
         r_irreg, b = read_beam(filename)
-        f = interp1d(np.append(-r_irreg, r_irreg), np.append(b, b), 'cubic', bounds_error=False, fill_value=(0., 0.))
-        inv_f = lambda x: f(x)-f(0.)/2
-        fwhm_beam = 2*optimize.newton(inv_f, x0=5.) 
+        f = interp1d(np.append(-r_irreg, r_irreg), np.append(b, b), 'cubic', bounds_error=False, fill_value=(0, 0))
+        inv_f = lambda x: f(x)-f(0)/2
+        fwhm_beam = 2*optimize.newton(inv_f, x0=5) 
     maxr = (maxr_data+3*fwhm_beam)//step*step
-    rad = np.arange(0., maxr+step, step)
+    rad = np.arange(0, maxr+step, step)
     rad = np.append(-rad[:0:-1], rad)
     rad_cut = rad[np.where(abs(rad) <= 3*fwhm_beam)]
     beam_mat = centdistmat(rad_cut)
@@ -151,23 +159,11 @@ def mybeam(step, maxr_data, approx=False, filename=None, normalize=True, fwhm_be
         beam_2d /= beam_2d.sum()*step**2
     return beam_2d, fwhm_beam
 
-def centdistmat(r, offset=0.):
-    '''
-    Create a symmetric matrix of distances from the radius vector
-    -------------------------------------------------------------
-    r = vector of negative and positive distances with a given step (center value has to be 0)
-    offset = value to be added to every distance in the matrix (default is 0)
-    ---------------------------------------------
-    RETURN: the matrix of distances centered on 0
-    '''
-    x, y = np.meshgrid(r, r)
-    return np.sqrt(x**2+y**2)+offset
-
-def read_tf(filename, approx=False, loc=0., scale=0.02, c=0.95):
+def read_tf(filename, approx=False, loc=0, scale=0.02, c=0.95):
     '''
     Read the transfer function data from the specified file
     -------------------------------------------------------
-    approx = whether to approximate or not the tf to the normal cdf (boolean, default is False)
+    approx = whether to approximate or not the tf to the normal cdf (True/False)
     loc, scale, c = location, scale and normalization parameters for the normal cdf approximation
     ---------------------------------------------------------------------------------------------
     RETURN: the vectors of wave numbers and transmission values
@@ -176,20 +172,6 @@ def read_tf(filename, approx=False, loc=0., scale=0.02, c=0.95):
     if approx:
         tf = c*norm.cdf(wn, loc, scale)
     return wn, tf
-
-def dist(naxis):
-    '''
-    Returns a matrix in which the value of each element is proportional to its frequency 
-    (https://www.harrisgeospatial.com/docs/DIST.html)
-    If you shift the 0 to the centre using fftshift, you obtain a symmetric matrix
-    ------------------------------------------------------------------------------------
-    naxis = number of elements per row and per column
-    -------------------------------------------------
-    RETURN: the (naxis x naxis) matrix
-    '''
-    axis = np.linspace(-naxis//2+1, naxis//2, naxis)
-    result = np.sqrt(axis**2+axis[:,np.newaxis]**2)
-    return np.roll(result, naxis//2+1, axis=(0, 1))
 
 def filt_image(wn_as, tf, side, step):
     '''
@@ -204,10 +186,24 @@ def filt_image(wn_as, tf, side, step):
     '''
     f = interp1d(wn_as, tf, 'cubic', bounds_error=False, fill_value=tuple([tf[0], tf[-1]])) # tf interpolation
     kmax = 1/step
+#    karr = dist(side)/side*kmax
     karr = dist(side)/side
     karr /= karr.max()
     karr *= kmax
-    return f(karr)
+    return f(karr)#(np.rot90(np.rot90(karr)))
+
+def dist(naxis):
+    '''
+    Returns a symmetric matrix in which the value of each element is proportional to its frequency 
+    (https://www.harrisgeospatial.com/docs/DIST.html)
+    ----------------------------------------------------------------------------------------------
+    naxis = number of elements per row and per column
+    -------------------------------------------------
+    RETURN: the (naxis x naxis) matrix
+    '''
+    axis = np.linspace(-naxis//2+1, naxis//2, naxis)
+    result = np.sqrt(axis**2+axis[:,np.newaxis]**2)
+    return np.roll(result, naxis//2+1, axis=(0, 1))
 
 class SZ_data:
     '''
@@ -245,15 +241,14 @@ class SZ_data:
         self.integ_mu = integ_mu
         self.integ_sig = integ_sig
 
-def log_lik(pars_val, pars, fit_pars, press, sz, output='ll'):
+def log_lik(pars_val, pars, press, sz, output='ll'):
     '''
     Computes the log-likelihood for the current pressure parameters
     ---------------------------------------------------------------
     pars_val = array of free parameters
     pars = set of pressure parameters
-    fit_pars = name of the parameters to fit
     press = pressure object of the class Pressure
-    sz = class of SZ data required
+    sz = class of SZ data
     output = desired output
         'll' = log-likelihood
         'chisq' = Chi-Squared
@@ -264,13 +259,13 @@ def log_lik(pars_val, pars, fit_pars, press, sz, output='ll'):
     RETURN: desired output or -inf when theta is out of the parameter space
     '''
     # update pars
-    press.update_vals(pars, fit_pars, pars_val)
+    press.update_vals(press.fit_pars, pars_val)
     # prior on parameters (-inf if at least one parameter value is out of the parameter space)
     parprior = sum((pars[p].prior() for p in pars))
     if not np.isfinite(parprior):
         return -np.inf
     # pressure profile
-    pp = press.press_fun(pars, sz.r_pp)
+    pp = press.press_fun(sz.r_pp)
     # abel transform
     ab = direct_transform(pp, r=sz.r_pp, direction='forward', backend='Python')
     # Compton parameter
@@ -309,15 +304,6 @@ def log_lik(pars_val, pars, fit_pars, press, sz, output='ll'):
         raise RuntimeError('Unrecognised output name (must be "ll", "chisq", "pp", "bright" or "integ")')
 
 def prelim_fit(sampler, pars, fit_pars, silent=False, maxiter=10):
-    '''
-    Preliminary fit on parameters to increase likelihood. Adapted from MBProj2
-    --------------------------------------------------------------------------
-    sampler = emcee EnsembleSampler object
-    pars = set of pressure parameters
-    fit_pars = name of the parameters to fit
-    silent = print output during fitting (boolean, default is False)
-    maxiter = maximum number of iterations (default is 10)
-    '''
     print('Fitting (Iteration 1)')
     ctr = [0]
     def minfunc(prs):
