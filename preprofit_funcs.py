223f0b18d3662e630f5e68d5294ceeec6b010a48
Fabio Castagna <fabio.castagna@inaf.it>
2021-04-23 16:22:31 +0200

    Update preprofit_funcs.py

--- preprofit_funcs.py
+++ preprofit_funcs.py
@@ -437,7 +437,8 @@ def log_lik(pars_val, press, sz, output='ll'):
     # Convolution with the beam and the transfer function at the same time
     map_out = np.real(ifftshift(ifft2(fft2(y_2d)*sz.filtering)))
     # Conversion from Compton parameter to mJy/beam
-    map_prof = (mean(map_out, labels=np.rint(sz.d_mat/sz.kpc_as/sz.step).astype(int), index=np.arange(sz.sep+1))*sz.conv_temp_sb+press.pars['pedestal'].val)*u.Unit('mJy beam-1')
+    map_prof = (mean(map_out, labels=np.rint(sz.d_mat/sz.kpc_as/sz.step).astype(int), index=np.arange(sz.sep+1))*
+                sz.conv_temp_sb+press.pars['pedestal'].val).to(sz.flux_data[1].unit)
     if output == 'bright':
         return map_prof
     g = interp1d(sz.radius[sz.sep:], map_prof, 'cubic', fill_value='extrapolate')
