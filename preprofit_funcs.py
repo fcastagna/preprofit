86c7ad82a898e8448e8fe081cb2f3737aef024d2
Fabio Castagna <fabio.castagna@inaf.it>
2021-10-07 10:15:57 +0200

    Update preprofit_funcs.py

--- preprofit_funcs.py
+++ preprofit_funcs.py
@@ -31,7 +31,8 @@ class Param:
         self.unit = unit
 
     def __repr__(self):
-        return '<Param: val=%.3g, minval=%.3g, maxval=%.3g, unit=%s, frozen=%s>' % (self.val, self.minval, self.maxval, self.unit, self.frozen)
+        return '<Param: val=%.3g, minval=%.3g, maxval=%.3g, unit=%s, frozen=%s>' % (
+            self.val, self.minval, self.maxval, self.unit, self.frozen)
 
     def prior(self):
         '''
