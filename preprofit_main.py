1c098750f72bbb9eb61fea967e6c9d2a400bf073
Fabio Castagna <fabio.castagna@inaf.it>
2024-12-10 18:49:42 +0100

    Update preprofit_main.py

--- preprofit_main.py
+++ preprofit_main.py
@@ -108,9 +108,9 @@ nk = len(press_knots)
 ## Model definition
 with pm.Model() as model:
     # Customize the prior distribution of the parameters using pymc distributions
-    pm.Uniform('sigmas', 0, 1, shape=nk)
+    pm.Uniform('sigmas_{int,k}', 0, 1, shape=nk)
     pm.Normal('log(P_k)', mu=press_knots, sigma=.5, initval=press_knots, shape=nk)
-    [pm.Normal('log(P_{%s,i})' % j, mu=model['log(P_k)'][j], sigma=model['sigmas'][j], shape=nc) for j in range(nk)]
+    [pm.Normal('log(P_{%s,i})' % j, mu=model['log(P_k)'][j], sigma=model['sigmas_{int,k}'][j], shape=nc) for j in range(nk)]
 
 # Sampling step
 mystep = 30.*u.arcsec # constant step (values larger than (1/7)*FWHM of the beam are not recommended)
