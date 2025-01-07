a2732b65f5979df5d9311808a0618d4cc8635b51
Fabio Castagna <fabio.castagna@inaf.it>
2025-01-07 13:02:33 +0100

    Update preprofit_main.py

--- preprofit_main.py
+++ preprofit_main.py
@@ -163,7 +163,7 @@ def main():
 
     with pm.Model() as model:
         # Customize the prior distribution of the parameters using pymc distributions
-        pm.Uniform('sigma_{int,k}', 0, 1, shape=nk)
+        pm.Uniform('sigma_{int,k}', 0, 1, initval=np.repeat(.2, nk), shape=nk)
         pm.Normal('lgP_k', mu=logunivpars, sigma=.5, initval=logunivpars, shape=nk)
         [pm.Normal('lgP_{%s,i}' % j, mu=model['lgP_k'][j], sigma=model['sigma_{int,k}'][j], shape=nc) for j in range(nk)]
         # Add pedestal component to the model
