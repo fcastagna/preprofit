191f13728837d72b9ed991accc7b143d666e0766
Fabio Castagna <fabio.castagna@inaf.it>
2026-03-05 18:02:16 +0100

    Update preprofit_main.py

--- preprofit_main.py
+++ preprofit_main.py
@@ -23,34 +23,34 @@ Om0 = 0.3 # Omega matter
 cosmology = FlatLambdaCDM(H0=H0, Om0=Om0)	
 
 # Cluster list
-clus = conf['clus']#['SPT-CLJ0500-5116', 'SPT-CLJ0637-4829', 'SPT-CLJ2055-5456'] # just 3 as an example, you need much more to determine the population parameters
+clus = conf['clus'] # just 3 clusters as an example, you need much more to determine the population parameters
 nc = len(clus)
-z = np.atleast_1d(conf['z'])#[.11, .2026, .139] # redshift list
+z = np.atleast_1d(conf['z']) # redshift list
 # Overdensity measures (set them for defining the starting point for the MCMC)
-r500 = np.atleast_1d(conf['r500'])*u.kpc#[943.85 207035, 1290.31531693, 1022.3744362]*u.kpc
+r500 = np.atleast_1d(conf['r500'])*u.kpc
 M500 = (4/3*np.pi*cosmology.critical_density(z).to(u.g/u.kpc**3)*500*r500**3).to(u.Msun)
 
 ## Beam and transfer function
 # Beam file already includes transfer function?
-beam_and_tf = conf["beam_and_tf"]#False
+beam_and_tf = conf["beam_and_tf"]
 # Beam and transfer function. From input data or Gaussian approximation?
 beam_approx = conf["beam_approx"]
 tf_approx = conf["tf_approx"]
 fwhm_beam = np.atleast_1d(conf["fwhm_beam"])*u.arcsec if conf["fwhm_beam"] is not None else conf["fwhm_beam"] # fwhm of the normal distribution, if adopted
-step_data = np.atleast_1d(conf["step_data"])*u.arcsec if conf["step_data"] is not None else conf["step_data"]
+step_data = np.atleast_1d(conf["step_data"])*u.arcsec if conf["step_data"] is not None else conf["step_data"] # radial step of beam data provided, if required
 loc, scale, k = None, None, None # location, scale and normalization parameters of the normal cdf for the transfer function approximation, if adopted
 
 # Transfer function provenance (not the instrument, but the team who derived it)
-tf_source_team = conf["tf_source_team"]#'SPT' # choose among 'NIKA', 'MUSTANG' or 'SPT'
+tf_source_team = conf["tf_source_team"] # choose among 'NIKA', 'MUSTANG' or 'SPT'
 
  ## File names (FITS and ASCII formats are accepted)
 # NOTE: if some of the files are not required, either assign a None value or just let them like this, preprofit will automatically ignore them
 # NOTE: if you have beam + transfer function in the same file, assign the name of the file to beam_filename and ignore tf_filename
 files_dir = conf["files_dir"] # files directory
-beam_filename = files_dir+conf["beam_filename"] if conf["beam_filename"] is not None else conf["beam_filename"]#None # beam
-tf_filename = files_dir+conf["tf_filename"] if conf["tf_filename"] is not None else conf["tf_filename"]#None # beam#'%s/sptsz_trough_filter_1d.dat' %files_dir # transfer function
-flux_filename = ['%s%s%s.dat' % (files_dir, conf["flux_filename"], c) for c in clus] if tf_source_team=='SPT' else [files_dir+conf["flux_filename"]]
-convert_filename = files_dir+conf["convert_filename"] if conf["convert_filename"] is not None else conf["convert_filename"]#None # beam#None # conversion Compton -> observed data
+beam_filename = files_dir+conf["beam_filename"] if conf["beam_filename"] is not None else conf["beam_filename"] # beam
+tf_filename = files_dir+conf["tf_filename"] if conf["tf_filename"] is not None else conf["tf_filename"] # transfer function
+flux_filename = ['%s%s%s.dat' % (files_dir, conf["flux_filename"], c) for c in clus] if tf_source_team=='SPT' else [files_dir+conf["flux_filename"]] # observed data
+convert_filename = files_dir+conf["convert_filename"] if conf["convert_filename"] is not None else conf["convert_filename"] # conversion Compton -> observed data
 
 # Temperature used for the conversion factor above
 t_const = 8*u.keV # if conversion is not required, preprofit ignores it
@@ -92,7 +92,7 @@ if press_mod == 'rcs':
 # 2. Generalized Navarro Frenk and White model
 if press_mod == 'gnfw':
     press = pfuncs.Press_gNFW(z=z, cosmology=cosmology, slope_prior=slope_prior, r_out=r_out, max_slopeout=max_slopeout)
-    pars_gnfw = conf['pars_gnfw']#['P_0', 'a', 'b', 'r_p'] # leave in this list only the parameters you want to fit
+    pars_gnfw = conf['pars_gnfw'] # leave in this list only the parameters you want to fit among ['P_0', 'a', 'b', 'r_p']
 # 3. Non parametric power-law model
 if press_mod == 'plaw':
     press = pfuncs.Press_nonparam_plaw(z=z, cosmology=cosmology, knots=knots, slope_prior=slope_prior, max_slopeout=max_slopeout)
@@ -102,9 +102,9 @@ if press_mod == 'cubspl':
 
 ## Get starting parameters of the population-averaged profile assuming an universal pressure profile
 logunivpars = np.mean(press.get_universal_params(M500=M500), axis=0)
-# if type(press)==pfuncs.Press_gNFW: 
-#     c500=1.177
-#     logunivpars[-1] = np.log10(10**(logunivpars[-1])/c500)
+if type(press)==pfuncs.Press_gNFW: 
+    c500=1.177
+    logunivpars[-1] = np.log10(10**(logunivpars[-1])/c500)
 nk = len(logunivpars) # number of parameters for the population-averaged pressure profile
 
 # If hierarchical model, do you want to include dependencies on redshift or mass?
@@ -113,7 +113,6 @@ M_dep = False # mass-dependent parameter?
 
 # Sampling step
 mystep = conf['mystep']*u.arcsec # constant step (values larger than (1/7)*FWHM of the beam are not recommended)
-# NOTE: when tf_source_team = 'SPT', be careful to adopt the same sampling step used for the transfer function
 
 # MCMC options
 niter = 4000 # number of iterations
@@ -132,11 +131,13 @@ def main():
     flux_data = [pfuncs.read_data(fl, ncol=3, units=flux_units) for fl in flux_filename] # radius, flux density, statistical error
 
     # Transfer function
-    wn_as, tf = [None, None] if beam_and_tf else pfuncs.read_tf(tf_filename, tf_units=tf_units, approx=tf_approx, loc=loc, scale=scale, k=k) # wave number, transmission
+    wn_as, tf = [None, None] if beam_and_tf else pfuncs.read_tf(
+		tf_filename, tf_units=tf_units, approx=tf_approx, loc=loc, scale=scale, k=k) # wave number, transmission
 
     # PSF+tf filtering
-    freq, fb, filtering = pfuncs.filtering(mystep, press.eq_kpc_as, maxr_data=maxr_data, approx=beam_approx, filename=beam_filename, beam_and_tf=beam_and_tf, 
-										   crop_image=crop_image, cropped_side=cropped_side, fwhm_beam=fwhm_beam, step_data=step_data, w_tf_1d=wn_as, tf_1d=tf, plotdir=plotdir)
+    freq, fb, filtering = pfuncs.filtering(mystep, press.eq_kpc_as, maxr_data=maxr_data, approx=beam_approx, filename=beam_filename, 
+										   beam_and_tf=beam_and_tf, crop_image=crop_image, cropped_side=cropped_side, fwhm_beam=fwhm_beam, 
+										   step_data=step_data, w_tf_1d=wn_as, tf_1d=tf, plotdir=plotdir)
     
     # Radius definition
     radius = np.arange(filtering.shape[0]//2+1)*mystep
@@ -155,7 +156,8 @@ def main():
         conv_temp_sb = 1*u.Unit('')
 
     # Set of SZ data required for the analysis
-    sz = pfuncs.SZ_data(clus=clus, step=mystep, eq_kpc_as=press.eq_kpc_as, conv_temp_sb=conv_temp_sb, flux_data=flux_data, radius=radius, sep=sep, r_pp=r_pp, r_am=r_am, filtering=filtering)
+    sz = pfuncs.SZ_data(clus=clus, step=mystep, eq_kpc_as=press.eq_kpc_as, conv_temp_sb=conv_temp_sb, flux_data=flux_data, radius=radius, sep=sep, 
+						r_pp=r_pp, r_am=r_am, filtering=filtering)
 
     # Compute P500
     press.P500 = [pfuncs.get_P500((sz.r_pp[j]/r500[j]).value, cosmology, z[j], M500=M500[j]).value for j in range(nc)]
@@ -221,17 +223,17 @@ def main():
             cloudpickle.dump(model, f, -1)
 
         ## Sampling
-        start_guess = [model['bright_%s' % j].eval({str(p): model.rvs_to_initial_values[model.named_vars[str(p)]] for p in model.free_RVs[-nk-1:]}) for j in range(nc)]
+        start_guess = [model['bright_%s' % j].eval({
+			str(p): model.rvs_to_initial_values[model.named_vars[str(p)]] for p in model.free_RVs[-nk-1:]}) for j in range(nc)]
         fact = 10**int(-np.sign(flux_data[0][1][0])*np.round(np.log10(np.abs(flux_data[0][1][0].value)), 0))
         pplots.plot_guess(start_guess, sz, press, fact=fact, plotdir=plotdir)
-        import pdb; pdb.set_trace()        
+
 		# Fit
         print("Fitting %s cluster%s" % (nc, 's' if nc>1 else ''))
         trace = pm.sample(draws=niter, tune=nburn, chains=nwalk, cores=os.cpu_count(), initvals=model.rvs_to_initial_values)
 	
 	    # Save chain
         trace.to_netcdf("%s/trace.nc" % savedir)
-
 
 if __name__ == '__main__':
     main()
